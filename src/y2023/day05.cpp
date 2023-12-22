b2c89236b0dbbaad77cccf3d7de0676166024820
SlimRunner <dv.polygon@gmail.com>
2023-12-21 23:47:19 -0800

    day 5 input parsed correctly

--- src/y2023/day05.cpp
+++ src/y2023/day05.cpp
@@ -1,12 +1,38 @@
 #include "EntryHeaders.hpp"
+#include "StringUtils.hpp"
 
 namespace {
 
 void solve(std::string path) {
   FileParser fp(path);
-  auto lines = fp.getLines();
+  auto const lines = fp.getLines();
+  auto line = lines.cbegin();
+  auto const EOL = lines.cend();
 
-  (void)lines; // unused variable
+  std::vector<std::vector<std::vector<int>>> maps;
+
+  auto seeds = parseInts(stringAfter(*(line++), ':'));
+  while (line != EOL && (line)->size() == 0) {line++;}
+  if (line != EOL) line++;
+  
+  while (line != EOL) {
+    maps.push_back({});
+    while (line != EOL && (line)->size() > 0) {
+      maps.back().push_back(parseInts(stringAfter(*line++, ':')));
+    }
+    while (line != EOL && (line)->size() == 0) {line++;}
+    if (line != EOL) line++;
+  }
+
+  for (auto i: maps) {
+    for (auto j: i) {
+      for (auto k: j) {
+        std::cout << k << " " << std::flush;
+      }
+      std::cout << std::endl;
+    }
+    std::cout << std::endl;
+  }
 
   std::cout << "P1: " << std::endl;
 }
