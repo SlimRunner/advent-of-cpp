4f62c7dba704642029c791e0c7fd4efc40435361
SlimRunner <dv.polygon@gmail.com>
2024-01-01 14:10:26 -0800

    visualize puzzle input

--- src/y2023/day10.cpp
+++ src/y2023/day10.cpp
@@ -1,12 +1,53 @@
 #include "EntryHeaders.hpp"
+#include <string>
+#include <string_view>
 
 namespace {
 
 void solve(std::string path) {
   FileParser fp(path);
   auto lines = fp.getLines();
+  // ║═╔╗╚╝
+  constexpr const std::string_view VERT_PIPE = "║";
+  constexpr const std::string_view HORZ_PIPE = "═";
+  constexpr const std::string_view CORN_SE_PIPE = "╔";
+  constexpr const std::string_view CORN_SW_PIPE = "╗";
+  constexpr const std::string_view CORN_NE_PIPE = "╚";
+  constexpr const std::string_view CORN_NW_PIPE = "╝";
+  for (auto const & line : lines) {
+    std::string wline;
+    for (auto const & chr : line) {
+      switch (chr) {
+      case '|':
+        wline.insert(wline.end(), VERT_PIPE.cbegin(), VERT_PIPE.cend());
+        break;
+      case '-':
+        wline.insert(wline.end(), HORZ_PIPE.cbegin(), HORZ_PIPE.cend());
+        break;
+      case 'L':
+        wline.insert(wline.end(), CORN_NE_PIPE.cbegin(), CORN_NE_PIPE.cend());
+        break;
+      case 'J':
+        wline.insert(wline.end(), CORN_NW_PIPE.cbegin(), CORN_NW_PIPE.cend());
+        break;
+      case '7':
+        wline.insert(wline.end(), CORN_SW_PIPE.cbegin(), CORN_SW_PIPE.cend());
+        break;
+      case 'F':
+        wline.insert(wline.end(), CORN_SE_PIPE.cbegin(), CORN_SE_PIPE.cend());
+        break;
+      case '.':
+        wline.push_back(' ');
+        break;
 
-  (void)lines; // unused variable
+      default:
+        wline.push_back(chr);
+        break;
+      }
+    }
+
+    std::cout << wline << std::endl;
+  }
 
   std::cout << "P1: " << std::endl;
 }
