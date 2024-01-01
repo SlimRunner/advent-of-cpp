c4e4d1111d8fd26e431d8b48a167672635d09034
SlimRunner <dv.polygon@gmail.com>
2024-01-01 14:21:00 -0800

    clean up visulization code

--- src/y2023/day10.cpp
+++ src/y2023/day10.cpp
@@ -4,46 +4,55 @@
 
 namespace {
 
-void solve(std::string path) {
-  FileParser fp(path);
-  auto lines = fp.getLines();
-  // ║═╔╗╚╝
+void insertFrom(std::string & out, const std::string_view & src) {
+  out.insert(out.end(), src.cbegin(), src.cend());
+}
+
+void beautifyPipes(std::string & outLine, char targetChar) {
   constexpr const std::string_view VERT_PIPE = "║";
   constexpr const std::string_view HORZ_PIPE = "═";
   constexpr const std::string_view CORN_SE_PIPE = "╔";
   constexpr const std::string_view CORN_SW_PIPE = "╗";
   constexpr const std::string_view CORN_NE_PIPE = "╚";
   constexpr const std::string_view CORN_NW_PIPE = "╝";
+
+  switch (targetChar) {
+  case '|':
+    insertFrom(outLine, VERT_PIPE);
+    break;
+  case '-':
+    insertFrom(outLine, HORZ_PIPE);
+    break;
+  case 'L':
+    insertFrom(outLine, CORN_NE_PIPE);
+    break;
+  case 'J':
+    insertFrom(outLine, CORN_NW_PIPE);
+    break;
+  case '7':
+    insertFrom(outLine, CORN_SW_PIPE);
+    break;
+  case 'F':
+    insertFrom(outLine, CORN_SE_PIPE);
+    break;
+  case '.':
+    outLine.push_back(' ');
+    break;
+
+  default:
+    outLine.push_back(targetChar);
+    break;
+  }
+}
+
+void solve(std::string path) {
+  FileParser fp(path);
+  auto lines = fp.getLines();
+  // ║═╔╗╚╝
   for (auto const & line : lines) {
     std::string wline;
     for (auto const & chr : line) {
-      switch (chr) {
-      case '|':
-        wline.insert(wline.end(), VERT_PIPE.cbegin(), VERT_PIPE.cend());
-        break;
-      case '-':
-        wline.insert(wline.end(), HORZ_PIPE.cbegin(), HORZ_PIPE.cend());
-        break;
-      case 'L':
-        wline.insert(wline.end(), CORN_NE_PIPE.cbegin(), CORN_NE_PIPE.cend());
-        break;
-      case 'J':
-        wline.insert(wline.end(), CORN_NW_PIPE.cbegin(), CORN_NW_PIPE.cend());
-        break;
-      case '7':
-        wline.insert(wline.end(), CORN_SW_PIPE.cbegin(), CORN_SW_PIPE.cend());
-        break;
-      case 'F':
-        wline.insert(wline.end(), CORN_SE_PIPE.cbegin(), CORN_SE_PIPE.cend());
-        break;
-      case '.':
-        wline.push_back(' ');
-        break;
-
-      default:
-        wline.push_back(chr);
-        break;
-      }
+      beautifyPipes(wline, chr);
     }
 
     std::cout << wline << std::endl;
