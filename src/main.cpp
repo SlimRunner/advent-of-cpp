cb236945799ec774eb1416115bf498f2d5b5421f
SlimRunner <dv.polygon@gmail.com>
2024-01-02 03:53:35 -0800

    add temporary perf profiling

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include "ArgParser.hpp"
 #include "Solutions.hpp"
+#include <chrono>
 #include <iostream>
 
 int main(int argc, char const *argv[]) {
   System::ArgParser args{argc, argv};
   auto const &sol = Solutions::getInstance(args);
+
+  using steady = std::chrono::steady_clock;
+  using chrono_ms = std::chrono::microseconds;
+
+  steady::time_point begin = steady::now();
 
   sol.runEntry(Year::y2023, Day::d01, PuzzleChoice::RUN_ALL);
   sol.runEntry(Year::y2023, Day::d02, PuzzleChoice::RUN_ALL);
@@ -16,5 +22,10 @@ int main(int argc, char const *argv[]) {
   sol.runEntry(Year::y2023, Day::d08, PuzzleChoice::RUN_ALL);
   sol.runEntry(Year::y2023, Day::d09, PuzzleChoice::RUN_ALL);
   sol.runEntry(Year::y2023, Day::d10, PuzzleChoice::RUN_ALL);
+
+  steady::time_point end = steady::now();
+  std::cout << "Above puzzles ran in ";
+  std::cout << std::chrono::duration_cast<chrono_ms>(end - begin).count() / 1000;
+  std::cout << " ms" << std::endl;
   return 0;
 }
